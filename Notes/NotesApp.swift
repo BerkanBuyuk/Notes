0f97e596c45d915c9ed23910b85dc1e6b09307d9
Berkan Büyük <berkanbyk55@gmail.com>
2022-12-03 19:05:17 +0300

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Berkan on 3.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
