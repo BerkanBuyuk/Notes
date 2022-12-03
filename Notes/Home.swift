51f6dcb83fd99f8bd06f5a7cec2cf708ce415061
Berkan Büyük <berkanbyk55@gmail.com>
2022-12-03 19:39:04 +0300

    Get API completed.

---
+++ Notes/Home.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  Notes
+//
+//  Created by Berkan on 3.12.2022.
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    @State var notes = [Note]()
+    
+    var body: some View {
+        
+        NavigationView {
+            List(self.notes) { note in
+                Text(note.note)
+                    .padding()
+            }
+            .onAppear(perform: {
+                fetchNotes()
+            })
+            .navigationTitle("Notes")
+            .navigationBarItems(trailing: Button(action: {
+                print("Add a note")
+            }, label: {
+                Text("Add")
+            }))
+        }
+    }
+    
+    func fetchNotes() {
+        let url = URL(string: "http://localhost:3000/notes")!
+        let task = URLSession.shared.dataTask(with: url) { data, res, err in
+            guard let data = data else {return}
+            
+            do {
+                let notes = try JSONDecoder().decode([Note].self, from: data)
+                self.notes = notes
+            } catch {
+                print(error)
+            }
+            
+        }
+        task.resume()
+    }
+}
+
+
+struct Note: Identifiable, Codable {
+    var id: String {_id}
+    var _id: String
+    var note: String
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
